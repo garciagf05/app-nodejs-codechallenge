c45a9ecd07108bd6b2750c8b54df68bc68bc1cb0
Fabián García <fgarcia@toolboxtve.com>
2024-10-12 20:14:37 -0500

    enhancing code in services

--- transaction-api/src/routes/account/account.service.ts
+++ transaction-api/src/routes/account/account.service.ts
@@ -1,55 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AccountDto } from './account.dto';
+import { getAllAccounts as getAllAccountsQuery } from './account.query';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AccountService {
-  constructor(private readonly httpService: HttpService) {}
+  private _apiGatewayPath: string;
+
+  constructor(private readonly httpService: HttpService) {
+    this._apiGatewayPath = process.env.API_GATEWAY_PATH;
+  }
 
   getAllAccounts(): Observable<AccountDto[]> {
-    const { API_GATEWAY_PATH } = process.env;
-    const query = `
-      query {
-        getAllAccounts {
-          id
-          owner
-          creationDate
-        }
-      }
-    `;
-
     try {
       const response = this.httpService
         .post(
-          `${API_GATEWAY_PATH}/gql`,
-          { query },
+          `${this._apiGatewayPath}/gql`,
+          { query: getAllAccountsQuery },
           { headers: { 'Content-Type': 'application/json' } },
         )
-        .pipe(
-          map((response) => {
-            const result = response.data;
-
-            if (result.errors && result.errors.length) {
-              throw new Error(
-                `Error creating account: ${JSON.stringify(response.data.errors)}`,
-              );
-            }
-
-            return result.data.getAllAccounts.map((data: AccountDto) => {
-              return {
-                id: data.id,
-                owner: data.owner,
-                creationDate: data.creationDate,
-              };
-            });
-          }),
-        );
+        .pipe(map(this._mapAllAccountsResponse));
 
       return response;
     } catch (error) {
       throw new Error(`Error creating account: ${error.message}`);
     }
+  }
+
+  private _mapAllAccountsResponse(response): AccountDto[] {
+    const result = response.data;
+
+    if (result.errors && result.errors.length) {
+      throw new Error(
+        `Error creating account: ${JSON.stringify(response.data.errors)}`,
+      );
+    }
+
+    return result.data.getAllAccounts.map((data: AccountDto) => {
+      return {
+        id: data.id,
+        owner: data.owner,
+        creationDate: data.creationDate,
+      };
+    });
   }
 }
--- transaction-api/src/routes/transaction/transaction.service.ts
+++ transaction-api/src/routes/transaction/transaction.service.ts
@@ -1,32 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { CreateTransactionDto, TransactionCreatedDto } from './transaction.dto';
+import { createTransaction as createTransactionQuery } from './transaction.query';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class TransactionService {
-  constructor(private readonly httpService: HttpService) {}
+  private _apiGatewayPath: string;
+
+  constructor(private readonly httpService: HttpService) {
+    this._apiGatewayPath = process.env.API_GATEWAY_PATH;
+  }
 
   createTransaction(
     createTransactionDto: CreateTransactionDto,
   ): Observable<TransactionCreatedDto> {
-    const { API_GATEWAY_PATH } = process.env;
-    const mutation = `
-      mutation($input: CreateTransactionInput!) {
-        createTransaction(input: $input) {
-          transactionExternalId: id
-          accountExternalIdDebit { id owner }
-          accountExternalIdCredit { id owner }
-          transferTypeId
-          transferType { name }
-          status { description }
-          value
-        }
-      }
-    `;
-
-    const vars = {
+    const variables = {
       input: {
         accountExternalIdDebit: createTransactionDto.accountExternalIdDebit,
         accountExternalIdCredit: createTransactionDto.accountExternalIdCredit,
@@ -38,36 +28,36 @@ export class TransactionService {
     try {
       const response = this.httpService
         .post(
-          `${API_GATEWAY_PATH}/gql`,
-          { query: mutation, variables: vars },
+          `${this._apiGatewayPath}/gql`,
+          { query: createTransactionQuery, variables },
           { headers: { 'Content-Type': 'application/json' } },
         )
-        .pipe(
-          map((response) => {
-            const result = response.data;
-
-            if (result.errors && result.errors.length) {
-              throw new Error(
-                `Error creating transaction: ${JSON.stringify(response.data.errors)}`,
-              );
-            }
-
-            const transaction = result.data.createTransaction;
-
-            return {
-              transactionExternalId: transaction.transactionExternalId,
-              accountExternalIdDebit: transaction.accountExternalIdDebit.id,
-              accountExternalIdCredit: transaction.accountExternalIdCredit.id,
-              transferTypeId: transaction.transferTypeId,
-              value: transaction.value,
-              status: transaction.status.id,
-            };
-          }),
-        );
+        .pipe(map(this._mapTransactionResponse));
 
       return response;
     } catch (error) {
       throw new Error(`Error creating transaction: ${error.message}`);
     }
+  }
+
+  private _mapTransactionResponse(response): TransactionCreatedDto {
+    const result = response.data;
+
+    if (result.errors && result.errors.length) {
+      throw new Error(
+        `Error creating transaction: ${JSON.stringify(response.data.errors)}`,
+      );
+    }
+
+    const transaction = result.data.createTransaction;
+
+    return {
+      transactionExternalId: transaction.transactionExternalId,
+      accountExternalIdDebit: transaction.accountExternalIdDebit.id,
+      accountExternalIdCredit: transaction.accountExternalIdCredit.id,
+      transferTypeId: transaction.transferTypeId,
+      value: transaction.value,
+      status: transaction.status.id,
+    };
   }
 }
