123017f388b35c35beddf962d825cff6f07d6842
Fabián García <fgarcia@toolboxtve.com>
2024-10-14 11:44:23 -0500

    Removed unused scripts

--- create-topics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cub kafka-ready -b kafka:29092 1 20
-
-kafka-topics --create --topic pendingQueue --bootstrap-server kafka:29092 --replication-factor 1 --partitions 1
-kafka-topics --create --topic validatedQueue --bootstrap-server kafka:29092 --replication-factor 1 --partitions 1
